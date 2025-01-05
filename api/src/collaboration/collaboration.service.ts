e97f00c53d58c142733153ebe270b306215e8143
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2025-01-05 15:06:09 +0800

    feat: add basic editor and collaboration

---
+++ api/src/collaboration/collaboration.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Hocuspocus } from "@hocuspocus/server";
+import { Database } from "@hocuspocus/extension-database";
+import { Logger } from "@hocuspocus/extension-logger";
+import { PrismaService } from "../_shared/database/prisma/prisma.service";
+
+@Injectable()
+export class CollaborationService implements OnModuleInit {
+  private hocuspocus: Hocuspocus;
+
+  constructor(private prisma: PrismaService) {}
+
+  async onModuleInit() {
+    this.hocuspocus = new Hocuspocus({
+      port: 5001,
+      name: "/collaboration",
+      extensions: [
+        new Logger(),
+        new Database({
+          fetch: async ({ documentName }) => {
+            const doc = await this.prisma.doc.findUnique({
+              where: { id: documentName },
+              select: { contentBinary: true },
+            });
+            return doc?.contentBinary ? new Uint8Array(doc.contentBinary) : null;
+          },
+          store: async ({ documentName, state }) => {
+            await this.prisma.doc.update({
+              where: { id: documentName },
+              data: { contentBinary: state },
+            });
+          },
+        }),
+      ],
+    });
+
+    await this.hocuspocus.listen();
+  }
+}
--- client/src/pages/doc/modules/detail/my-doc.tsx
+++ client/src/pages/doc/modules/detail/my-doc.tsx
@@ -3,11 +3,10 @@ import { useDocumentStore } from "../../stores/store";
 import { Textarea } from "@/components/ui/textarea";
 import { Toolbar } from "./toolbar";
 import Cover from "./cover";
+import TiptapEditor from "../editor";
 
 export default function DocDetail() {
   const { currentDocument } = useCurrentDocument();
-  const updateDocument = useDocumentStore.use.updateDocument();
-  const treeData = useDocumentStore.use.treeData();
 
   if (!currentDocument) return null;
 
@@ -16,12 +15,7 @@ export default function DocDetail() {
       {currentDocument?.coverImage && <Cover cover={currentDocument.coverImage} />}
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto ">
         <Toolbar doc={currentDocument} preview={false} />
-        <Textarea
-          value={JSON.stringify(treeData, null, 2) || ""}
-          onChange={(e) => updateDocument(currentDocument.id, { content: e.target.value })}
-          placeholder="Start writing..."
-          className="min-h-[400px] resize-none mt-5"
-        />
+        <TiptapEditor id={currentDocument.id} />
       </div>
     </div>
   );
