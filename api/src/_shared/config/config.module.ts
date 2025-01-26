77a617bfe85efb936bc5a207e0ff00f893a864d8
yorkchan6228 <yorkchan6228@gmail.com>
2025-01-26 12:43:09 +0800

    feat: add dotenv-vault

--- api/src/_shared/config/config.module.ts
+++ api/src/_shared/config/config.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import * as config from "./configs";
 import { validateConfig } from "./config-validation";
+import path from "node:path";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [`${process.cwd()}/.env`, `${process.cwd()}/.env.${process.env.NODE_ENV}`],
+      envFilePath: [`${process.cwd()}/../.env`, `${process.cwd()}/../.env.${process.env.NODE_ENV}`],
       // Load env files from most specific to least specific
       expandVariables: true,
       load: [...Object.values(config)],
