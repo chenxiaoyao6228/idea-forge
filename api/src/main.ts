43c73ed5cffb8dbf8b1ccdc5e78b117242467cdb
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2025-02-13 18:21:12 +0800

    feat: add request rate limit and real ip checker

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,11 +9,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import type { NestExpressApplication } from "@nestjs/platform-express";
 import * as session from "express-session";
+import { mw as requestIpMw } from "request-ip";
 import * as cookieParser from "cookie-parser";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { ConfigService } from "@nestjs/config";
 import { AllExceptionsFilter } from "./_shared/filters/all-exception-filter";
 import { HttpExceptionFilter } from "./_shared/filters/http-exception-filter";
+import rateLimit from "express-rate-limit";
 declare const module: any;
 
 async function bootstrap() {
@@ -30,6 +32,15 @@ async function bootstrap() {
   });
 
   const configService = app.get(ConfigService);
+  // Get Real IP address
+  app.use(requestIpMw({ attributeName: "ip" }));
+  // Rate limit
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // limit each IP to 1000 requests per windowMs
+    }),
+  );
 
   // Filters
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
