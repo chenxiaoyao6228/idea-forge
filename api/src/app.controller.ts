9c144d373b1ac213f4f875c38c6c273881b6af76
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2025-03-01 02:42:19 +0800

    feat: add i18n for nest

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Body, Post, Req, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "./auth/guards/jwt-auth.guard";
 import { Public } from "./auth/decorators/public.decorator";
+import { AppService } from "./app.service";
+import { I18n, I18nContext } from "nestjs-i18n";
+import { I18nTranslations } from "./_generated/i18n.generated";
 
 @Controller("/api")
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Public()
   @Get("health")
   async health() {
@@ -20,5 +25,14 @@ export class AppController {
   @Get("protected")
   async protected(@Req() req: any) {
     return "protected " + req.user.id;
+  }
+
+  @Public()
+  @Get("/test-i18n")
+  // async testI18n() {
+  //   return this.appService.getHello();
+  // }
+  async getHello(@I18n() i18n: I18nContext<I18nTranslations>): Promise<string> {
+    return await i18n.t("translation.test", { args: { name: "Toon" } });
   }
 }
