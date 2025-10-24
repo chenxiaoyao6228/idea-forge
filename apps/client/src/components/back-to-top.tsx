b9f676d1d2f254003f6a4ddf39f55eb05cf5b866
YorkChan6228 <yorkchan6228@gmail.com>
2025-10-24 16:34:06 +0800

    style: update some ui style

--- apps/client/src/components/back-to-top.tsx
+++ apps/client/src/components/back-to-top.tsx
@@ -38,12 +38,12 @@ export default function BackToTop() {
           <Button
             variant="outline"
             onClick={scrollToTop}
-            className={cn(
-              "fixed right-4 bottom-10 rounded-full px-2 z-50 transition-all duration-300 select-none",
-              commentsSidebarOpen && `mr-[${COMMENT_SIDEBAR_WIDTH + 10}px]`,
-            )}
+            className={cn("fixed right-4 bottom-10 rounded-full px-2 z-50 transition-all duration-300 select-none")}
+            style={{
+              marginRight: commentsSidebarOpen ? `${COMMENT_SIDEBAR_WIDTH + 10}px` : "0px",
+            }}
           >
-            <Rocket className="h-8 w-8" />
+            <Rocket className="h-8 w-8" style={{ transform: "rotate(-45deg)" }} />
           </Button>
         </TooltipTrigger>
         <TooltipContent align="center">
